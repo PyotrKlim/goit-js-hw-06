e8e9e3f5cf2016e890d4a34757ae30052dc57db6
PyotrKlim <piter.com@ukr.net>
2022-12-11 17:31:40 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,33 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const boxGallery = document.querySelector("#boxes");
+const inputElem = document.querySelector("input");
+const btnCreate = document.querySelector("button[data-create]");
+
+const fun = function creteNewArr() {
+  const elements = [];
+
+  for (let i = 1; i <= inputElem.value; i++) {
+    const newDiv = document.createElement("div");
+    const parametrDiv = `${20 + i * 10}px`;
+
+    newDiv.style.width = parametrDiv;
+    newDiv.style.height = parametrDiv;
+    newDiv.style.background = getRandomHexColor();
+    newDiv.style.border = `3px solid ${getRandomHexColor()}`;
+
+    elements.push(newDiv);
+  }
+
+  boxGallery.append(...elements);
+};
+
+btnCreate.addEventListener("click", fun);
+
+const btnDestroy = document.querySelector("button[data-destroy]");
+
+btnDestroy.addEventListener("click", () => {
+  boxGallery.innerHTML = "";
+});
